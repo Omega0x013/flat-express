d7b253099d01949c7856417d1673cfbfed10ce37
Omega0x013 <xanderbielby@gmail.com>
2021-10-12 14:58:51 +0100

    change to 405

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const r404 = (_, res) => res.sendStatus(404);
+const r405 = (_, res) => res.sendStatus(405);
 
 /**
  * Takes a set of routes and mounts them to your express app.
@@ -19,14 +19,14 @@ module.exports = function flat(app, routes) {
     // This has no safety checks on purpose
     for (let [path, view] of Object.entries(routes)) {
         app.route(path)
-            .get(view['get'] || r404)
-            .head(view['head'] || r404)
-            .post(view['post'] || r404)
-            .put(view['put'] || r404)
-            .delete(view['delete'] || r404)
-            .connect(view['connect'] || r404)
-            .options(view['options'] || r404)
-            .trace(view['trace'] || r404)
-            .patch(view['patch'] || r404);
+            .get(view['get'] || r405)
+            .head(view['head'] || r405)
+            .post(view['post'] || r405)
+            .put(view['put'] || r405)
+            .delete(view['delete'] || r405)
+            .connect(view['connect'] || r405)
+            .options(view['options'] || r405)
+            .trace(view['trace'] || r405)
+            .patch(view['patch'] || r405);
     }
 }
