b0583129a9e43161d150383933b59e9106e07e5e
Omega0x013 <xanderbielby@gmail.com>
2021-10-18 13:04:31 +0100

    add safety net

--- index.js
+++ index.js
@@ -3,8 +3,18 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import express from 'express';
+import pathm from 'path'; // path-module
 
 const r405 = (_, res) => res.sendStatus(405);
+
+/**
+ * 
+ * @param {string} target Path
+ * @returns {object}
+ */
+export function file(target) {
+    return {get: (req, res) => res.sendFile(pathm.join(pathm.resolve() + target))}
+}
 
 /**
  * Takes a set of routes and mounts them to your express app.
@@ -14,27 +24,27 @@ const r405 = (_, res) => res.sendStatus(405);
  * {get, head, post, put, delete, connect, options, trace, patch}
  * ```
  * 
- * @param {any} routes Map of `[String]: [Object | String]`. The properties of
-   the object listed above are used as route handlers, duplicates are ignored. A string may be used which sets up a 302 route to the new path.
+ * @param routes Map of `[String]: [Object | String]`. The properties of
+   the object listed above are used as route handlers, duplicates are ignored.
+   A string may be used which creates a GET that reads from a path.
  * @returns {Router} Router object
 */
 export default function flat(routes) {
     // This has no safety checks on purpose
     let router = express.Router();
     for (let [path, view] of Object.entries(routes)) {
-        if (typeof view  === 'string') {
-            router.get      (path, (req, res) => res.redirect(view));
-        } else {
-            router.get      (path, view['get']      || r405);
-            router.head     (path, view['head']     || r405);
-            router.post     (path, view['post']     || r405);
-            router.put      (path, view['put']      || r405);
-            router.delete   (path, view['delete']   || r405);
-            router.connect  (path, view['connect']  || r405);
-            router.options  (path, view['options']  || r405);
-            router.trace    (path, view['trace']    || r405);
-            router.patch    (path, view['patch']    || r405);
-        }
+        console.log(view);
+        console.log(typeof view);
+        if (typeof view === 'string') view = file(view);
+        router.get      (path, view['get']      || r405);
+        router.head     (path, view['head']     || r405);
+        router.post     (path, view['post']     || r405);
+        router.put      (path, view['put']      || r405);
+        router.delete   (path, view['delete']   || r405);
+        router.connect  (path, view['connect']  || r405);
+        router.options  (path, view['options']  || r405);
+        router.trace    (path, view['trace']    || r405);
+        router.patch    (path, view['patch']    || r405);
     }
     return router;
 }
